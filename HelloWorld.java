71497bcf5bbace050d56eb53bcdfae06fb3ff74f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-23 08:48:57 -0800

    initialize

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// ////////////////////////////////////////////////////
+// JAVA > HELLO WORLD
+// Virgilio D. Cabading
+// ////////////////////////////////////////////////////
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String hello = "Hellow World!";
+        System.out.println(hello);
+    }
+
+    public static String sayHello() {
+        return "Hello World!";
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Java in 100 Seconds Read Me
+
+This is a hello world program for Java using the code from youtube's Java in 100 seconds.
+I'm using this as a starting leg to start learning JAVA.
